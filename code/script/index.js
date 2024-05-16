4867d17adcbe7e3f18dd5b267114709ba01b5ba1
FloDev <43553244+Ultiminer@users.noreply.github.com>
2024-05-17 00:24:41 +0200

    Added start of login logic

---
+++ code/script/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+const signIn=document.querySelector('.button_left');
+const signUp=document.querySelector('.button_right');
+const UserName=document.getElementById('lname');
+const Password=document.getElementById('lpw');
+
+const checkUser=function(username, password){
+    if(username==='admin'&&password==='123')return true;
+    return false; 
+}
+const registerUser=function(username, password){
+    return 0;
+}
+const userExists=function(username){
+    return 0;
+}
+
+let userState=0;
+signIn.addEventListener('click',function(){
+    signIn.textContent='clicked:)';
+    if(checkUser(UserName.value,Password.value))
+        {
+            userState=1; 
+            signIn.textContent='logged in :)';
+        }
+});
+signUp.addEventListener('click',function(){
+    signUp.textContent='clicked:)';
+    if(userExists(UserName.value))registerUser(UserName.value,Password.value);
+});
+
+
